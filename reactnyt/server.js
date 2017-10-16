9374acc333725b0a1fb5322ad267768280429610
rmeaney <rjmeaney@gmail.com>
2017-10-16 15:08:14 -0400

    fixes

--- reactnyt/server.js
+++ reactnyt/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
 //mongoose.connect('mongodb://localhost/nyt-reactsearch');
 mongoose.connect('mongodb://localhost/reactScrape');
